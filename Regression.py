59e3b21d3c4617f0959c101c9d55d38063ced2fb
cerentkn04 <cerentkn04@gmail.com>
2025-01-09 21:12:00 +0300

    gradient

--- Regression.py
+++ Regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sympy import *
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
@@ -18,27 +19,56 @@ def LinReg( X_train, y_train):
     X_train = np.array( X_train)
     y_train = np.array(y_train)
     n= len(X_train)
- 
-    sumX = np.sum(X_train)
-    sumX2 = np.sum(X_train**2)
-    sumY = np.sum(y_train)
-    sumXY = np.sum(X_train * y_train)
-  
     
-    b=(n * sumXY - sumX * sumY)/(n * sumX2 - sumX**2)
-    a=(sumY -b*sumX)/n
+    b=1
+    a=0
     sol= a+ b*X_train
+    print("-----------------------------------sol",sol)
+    ssr(a,b,X_train,y_train)
+    intercept,slope ,sol = gradientD(X_train,y_train,0.1,)
 
-    return a,b,sol
+ 
+
+    return intercept,slope,sol
 
 def var(array):
     return np.sum((x-np.mean(array))**2 for x in array)/len(array)
 
+def gradientD(x,actual,LL,max_iters=10, tolerance=1e-6):
+    intercept=0
+    slope = 0.0
+    a, b = symbols('a b')
+    ssr = sum((actual - (a + b * x))**2 for actual, x in zip(actual, x))
+    difa = diff(ssr,a)
+    difb = diff(ssr,b)
+    for _ in range(max_iters):
+        grad_a=difa.subs({a:intercept,b:slope})
+        grad_b=difb.subs({a:intercept,b:slope})
+        intercept -= LL * grad_a
+        slope -= LL * grad_b
+        ssr =sum((actual - ( intercept+ slope * x))**2 for actual, x in zip(actual, x))
+        
+    
+    return intercept, slope, ssr
+
+
+     
+
 def Rsquare(predicted,actual):
+    print(predicted)
     ss_total =np.sum((predicted- np.mean(actual))**2)
     ss_residual = np.sum((actual- predicted)**2)
     return 1-(ss_residual/ss_total)
 
-a,b,sol= LinReg(X_train, y_train)
-plt.plot(X_train, sol, color="red", label=f"Regression Line: y = {a:.2f} + {b:.2f}x")
+def ssr(a,b,x,actual):
+    return np.sum((actual-(a+b*x))**2)
+intercept, slope, sol = LinReg(X_train, y_train)
+
+# Evaluating intercept and slope to numeric values (use .evalf())
+intercept_value = intercept.evalf()
+slope_value = slope.evalf()
+
+# Plotting the regression line
+plt.plot(X_train, sol, color="red", label=f"Regression Line: y = {intercept_value:.2f} + {slope_value:.2f}x")
+plt.legend()
 plt.show()
