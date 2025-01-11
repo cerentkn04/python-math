03d7344e47430477ac7849b6dd26115fba55f17c
cerentkn04 <cerentkn04@gmail.com>
2025-01-11 20:12:45 +0300

    polynomial regression

--- Regression1.py
+++ Regression1.py
@@ -8,23 +8,58 @@ data= pd.read_csv("icecraemSellingData.csv")
 x=data['Temperature (°C)']
 y=data['Ice Cream Sales (units)']
 
-
+sns.pairplot(data, x_vars='Temperature (°C)', y_vars='Ice Cream Sales (units)', height=4, kind='scatter')  
 def PolReg(x,y):
     x_vals=np.array(x)
     y_vals=np.array(y)
     n_samp =np.shape(x_vals)
-    X_train = np.column_stack((np.ones(n_samp), x_vals)) 
-    w = np.zeros(X_train.shape[1])
-    print(len(w))
-    print(len(X_train))
+    solution=[]
 
-    solution = np.dot(X_train, w) + 0
-
+    Xf=[]
+    for i in range(len(x_vals)):
+        row=[]
+        for j in range(3):
+            row.append(x_vals[i]**j)
+        Xf.append(row)
     
+    w = np.zeros(3)
+    bias=0 
+    solution = np.dot(Xf,w) + bias
    
-   
-    
 
-    return x_vals
+    w= Gradient(Xf,w,solution,y_vals)
+   
+    yp = np.dot(Xf,w) + bias
+
+    return yp
+
+def Gradient(xf,w,predicted,actual,lr=1e-5,max_iters=1000):
+    Xf = np.array(xf)
+    Xf_t = Xf.T
+    delta = predicted - actual
+    for _ in range(max_iters):
+        dw = (-1 / len(xf)) * np.dot(Xf_t, delta)
+        w += lr * dw 
+    
+    return w
+
+def solcompute(Xf,W):
+    yp=[]
+    for i in range(len(Xf)):
+        yp.append(np.dot(Xf[i], W))
+    return yp
 
 solution= PolReg(x,y)
+
+print(solution)
+
+
+
+
+# Plotting the results
+plt.scatter(x, y, color="blue", label="Data points")  # Scatter plot of the data
+plt.plot(x, solution, color="red", label="Polynomial Regression Line")  # Regression line
+plt.xlabel("Temperature (°C)")
+plt.ylabel("Ice Cream Sales (units)")
+plt.legend()
+plt.show()
