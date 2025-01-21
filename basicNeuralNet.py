3105860c0b61c2bda7b7df879fb0d486326a7a44
cerentkn04 <cerentkn04@gmail.com>
2025-01-21 21:53:40 +0300

    pytorch cont
    first pytorch code contiuned

--- basicNeuralNet.py
+++ basicNeuralNet.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 data = pd.read_csv("iris.csv")
@@ -67,6 +68,7 @@ class Perceptron:
         self.b_ = np.float64(0.)
         self.errors_ = []
 
+
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X, y):
@@ -85,6 +87,29 @@ class Perceptron:
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, 0)
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+    np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+        y=X[y == cl, 1],
+        alpha=0.8,
+        c=colors[idx],
+        marker=markers[idx],
+        label=f'Class {cl}',
+        edgecolor='black')
 
 ppn = Perceptron(eta=0.1,n_iter=10)
 ppn.fit(X,y)
@@ -92,4 +117,10 @@ ppn.fit(X,y)
 plt.plot(range(1,len(ppn.errors_)+1), ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
+plt.show()
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('Sepal length [cm]')
+plt.ylabel('Petal length [cm]')
+plt.legend(loc='upper left')
 plt.show()
