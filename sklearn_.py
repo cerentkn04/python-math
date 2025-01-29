bb44710bb198689958dc62086b8c10c29de24b09
cerentkn04 <cerentkn04@gmail.com>
2025-01-29 17:17:55 +0300

    svm

--- sklearn_.py
+++ sklearn_.py
@@ -5,22 +5,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron,LogisticRegression
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 iris = datasets.load_iris()
-
-X = iris.data[:, [2, 3]]
-y = iris.target
-print('Class labels:', np.unique(y))
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
-
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
-ppn = Perceptron(eta0=0.1, random_state=1)
-ppn.fit(X_train_std, y_train)
-y_pred = ppn.predict(X_test_std)
-print('Misclassified examples: %d' % (y_test != y_pred).sum())
-
 
 
 def plot_decision_regions(X, y, classifier, test_idx=None,resolution=0.02):
@@ -56,29 +42,51 @@ def plot_decision_regions(X, y, classifier, test_idx=None,resolution=0.02):
         linewidth=1, marker='o',
         s=100, label='Test set')
 
-X_combined_std = np.vstack((X_train_std, X_test_std))
-y_combined = np.hstack((y_train, y_test))
-X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
-y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
-lr= LogisticRegression(C=100.0,solver='lbfgs',multi_class='ovr')
-lr.fit(X_train_std,y_train)
-plot_decision_regions(X_combined_std, y_combined,classifier=lr,test_idx=range(105,150))
-plt.xlabel('Petal length [standardized]')
-plt.ylabel('Petal width [standardized]')
-plt.legend(loc='upper left')
-plt.tight_layout()
-plt.show()
-weights, params = [], []
-for c in np.arange(-5, 5):
-    lr= LogisticRegression(C=10.**c,multi_class='ovr')
+if __name__ == "__main__":
+
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+    print('Class labels:', np.unique(y))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+    ppn = Perceptron(eta0=0.1, random_state=1)
+    ppn.fit(X_train_std, y_train)
+    y_pred = ppn.predict(X_test_std)
+    print('Misclassified examples: %d' % (y_test != y_pred).sum())
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+    X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+    y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+    lr= LogisticRegression(C=100.0,solver='lbfgs',multi_class='ovr')
     lr.fit(X_train_std,y_train)
-    weights.append(lr.coef_[1])
-    params.append(10.**c)
-weights = np.array(weights)
-plt.plot(params,weights[:,0],label='petal lenght')
-plt.plot(params,weights[:,1],label='PETAL Width',linestyle='--')
-plt.ylabel('weight coefficient')
-plt.xlabel('c')
-plt.legend(loc='upper left')
-plt.xscale('log')
-plt.show()
+    plot_decision_regions(X_combined_std, y_combined,classifier=lr,test_idx=range(105,150))
+    plt.xlabel('Petal length [standardized]')
+    plt.ylabel('Petal width [standardized]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
+    weights, params = [], []
+    for c in np.arange(-5, 5):
+        lr= LogisticRegression(C=10.**c,multi_class='ovr')
+        lr.fit(X_train_std,y_train)
+        weights.append(lr.coef_[1])
+        params.append(10.**c)
+    weights = np.array(weights)
+    plt.plot(params,weights[:,0],label='petal lenght')
+    plt.plot(params,weights[:,1],label='PETAL Width',linestyle='--')
+    plt.ylabel('weight coefficient')
+    plt.xlabel('c')
+    plt.legend(loc='upper left')
+    plt.xscale('log')
+    plt.show()
+    svm = SVC(kernel='linear',C=1.0,random_state=1)
+    svm.fit(X_train_std, y_train)
+    plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105,150))
+    plt.xlabel('petal lenght [standarized]')
+    plt.ylabel('petal lenght [standarized]')
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
