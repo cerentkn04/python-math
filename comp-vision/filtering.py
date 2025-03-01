7b98fd0cff0e6cb8e13b0060b93a107a8829af7e
cerentkn04 <cerentkn04@gmail.com>
2025-03-01 14:25:28 +0300

    steerable_base

--- comp-vision/filtering.py
+++ comp-vision/filtering.py
@@ -2,34 +2,59 @@ import numpy as np
 from PIL import Image
 import cv2 as cv
 
-# Open and resize image using PIL
-image = Image.open("sample2.jpg")
+
+image = Image.open("sample3.jpg")
 image = image.resize((500, 500))
 ddepth = cv.CV_16S
 
-# Convert PIL image to NumPy array (OpenCV format)
 image = np.array(image)
 
-# Convert RGB to BGR for OpenCV
 image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
 
-# Show the original image (now in OpenCV format)
+
 cv.imshow("Original Image", image)
 
-# Define a 5x5 blur kernel
-kernel = np.ones((5, 5), np.float32) / 25
-image_gray=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-# Apply filter2D for blurring
+
+kernel = np.ones((10, 10), np.float32) / 100
+kernel1 = np.ones((5, 5), np.float32)/30
+kernel1y = kernel1.T
+
+image_gray=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+normalizedimg =cv.normalize(image, None, 0, 1, cv.NORM_MINMAX, dtype=cv.CV_32FC1)
+filtered_x = cv.filter2D(normalizedimg,ddepth= -1, kernel =kernel1)
+filtered_y = cv.filter2D(normalizedimg, ddepth= -1, kernel =kernel1y)
+
+theta_deg = 45
+m=200
+theta = np.radians(theta_deg)
+
+x_index, y_index = np.meshgrid(np.arange(-m, m+1), np.arange(-m, m+1))
+
+
+
+G = lambda x, y: np.exp(-(x**2 + y**2))
+G0 = lambda x, y: -2*x*G(x,y)
+G90 = lambda x, y: 2*y*G(x,y)
+
+gk = G(x_index, y_index)
+gk0 = G0(x_index, y_index)
+gk90 = G90(x_index, y_index)
+
+
+gk_theta = np.cos(theta)*gk0 + np.sin(theta)*gk90
+
 blur = cv.filter2D(image, -1, kernel)
-Gblur = cv.GaussianBlur(image,(5,5),21)
 LaplacePic = cv.Laplacian(image_gray, ddepth, 3)
 abs_dst = cv.convertScaleAbs(LaplacePic)
-# Show the blurred image
-cv.imshow("Blurred Image", blur)
-cv.imshow("gaussian Image", Gblur)
-cv.imshow("Laplace Image", abs_dst)
 
-# Wait for a key press to close windows
+R_Theta = cv.filter2D(normalizedimg, ddepth, kernel=gk_theta)
+
+
+
+cv.imshow("current Image", R_Theta)
+
+
+
 cv.waitKey(0)
 cv.destroyAllWindows()
