92e4584a3674a3a0765e31ece77ad92cfe6d65b0
cerentkn04 <cerentkn04@gmail.com>
2025-03-09 15:44:05 +0300

    sift
    can detect given object regardless of its orientation or size  in the search image .

--- comp-vision/SIFT/sift.py
+++ comp-vision/SIFT/sift.py
@@ -1,30 +1,43 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-from Sift_Operations import *
 
-image1 = cv2.imread('eif1.jpg') 
-image2 = cv2.imread('eif2.jpg') 
-
-
-height, width = image1.shape[:2]
-image2 = cv2.resize(image2, (width, height)) 
+object_img = cv.imread('photo3.jpeg')
+scene_img= cv.imread('photo4.jpeg')
 
 
-Image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-Image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+sift = cv.SIFT_create()
 
 
-sift = cv2.xfeatures2d.SIFT_create()
-
-keypoints_1, descriptors_1 = sift.detectAndCompute(image1,None)
-keypoints_2, descriptors_2 = sift.detectAndCompute(image2,None)
-bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
-
-matches = bf.match(descriptors_1,descriptors_2)
-matches = sorted(matches, key = lambda x:x.distance)
-img3 = cv2.drawMatches(image1, keypoints_1, image2, keypoints_2, matches[:50], image2, flags=2)
-plt.imshow(img3),plt.show()
+kp1, des1 = sift.detectAndCompute(object_img, None)
+kp2, des2 = sift.detectAndCompute(scene_img, None)
 
 
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=1000)
 
+
+flann = cv.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+
+good_matches = []
+for m, n in matches:
+    if m.distance < 0.7 * n.distance:
+        good_matches.append(m)
+
+num_keypoints = len(good_matches)
+result = {
+    "status": "PASS" if num_keypoints >= 1000 else "FAIL",
+    "keypoints_detected": num_keypoints
+}
+
+img_matches = cv.drawMatches(object_img, kp1, scene_img, kp2, good_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+cv.imshow("show",img_matches)
+print(result)
+
+
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
