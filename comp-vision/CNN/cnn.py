430fe27121b7c0a04d8684b47d6af2549505e019
cerentkn04 <cerentkn04@gmail.com>
2025-03-15 15:31:13 +0300

    a slow cnn

--- comp-vision/CNN/cnn.py
+++ comp-vision/CNN/cnn.py
@@ -1,51 +1,113 @@
-import numpy as np
 import torch
 import torch.nn as nn
-from PIL import Image
-import cv2 as cv 
 from torchvision import transforms
-import matplotlib.pyplot as plt
+import torchvision
+import torch.optim as optim
+import pandas as pd
+import os
+import cv2
+from torch.utils.data import Dataset, DataLoader
+
+# Dataset Class
+class Fer2013Dataset(Dataset):
+    def __init__(self, path, transform=None):
+        self.data = []
+        self.labels = []
+        self.transform = transform
+        self.classes = os.listdir(path)
+
+        for emotion_class in self.classes:
+            class_index = self.classes.index(emotion_class)
+            folder_path = os.path.join(path, emotion_class)
+
+            for image_name in os.listdir(folder_path):
+                image_path = os.path.join(folder_path, image_name)
+                img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Grayscale images
+
+                if img is not None:
+                    img = cv2.resize(img, (48, 48))  # Resize all images to 48x48
+                    self.data.append(img)
+                    self.labels.append(class_index)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image = self.data[idx]
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
 
 
-image=Image.open("../sample2.jpg")
-
+# Transformations
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToPILImage(),
+    transforms.Grayscale(num_output_channels=1),  # Ensure single-channel image
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
 ])
 
-image = transform(image).unsqueeze(0)
-image = image
-conv1 = nn.Conv2d(3, 64, kernel_size=(3,3), stride=1, padding=1)
-act1 = nn.ReLU()
-drop1 = nn.Dropout(0.3)
- 
-conv2 = nn.Conv2d(64, 32, kernel_size=(3,3), stride=1, padding=1)
-act2 = nn.ReLU()
-pool2 = nn.MaxPool2d(kernel_size=(2, 2))
-flatten = nn.Flatten()
+# Load Dataset
+batch_size = 32
+train_dataset = Fer2013Dataset('./arch/train', transform=transform)
+test_dataset = Fer2013Dataset('./arch/test', transform=transform)
 
-P1 = nn.MaxPool2d(3, stride=2)
-imm= act1(conv1(image)) 
-imm= drop1(imm)
-img= act2(conv2(imm))
-img =pool2(img)
-flatten_filters= flatten(img)
-linear= nn.Linear(flatten_filters.shape[1], 30)
-linear_output= linear(flatten_filters)
+trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-print("FLATTT", linear_output)
+# Model
+class Fer2013(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=1, padding=1)  # 1 input channel for grayscale
+        self.act1 = nn.ReLU()
+        self.drop1 = nn.Dropout(0.3)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=(3, 3), stride=1, padding=1)
+        self.act2 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2, stride=2)  # Reduce feature map size
+        self.flatten = nn.Flatten()
+        self.fc4 = nn.Linear(32 * 24 * 24, 10)  # Adjusted FC layer input size
 
-num_filters = linear_output.shape[1] 
-rows, cols = 2, 4  
+    def forward(self, dataset):
+        dataset = self.act1(self.conv1(dataset))
+        dataset = self.drop1(dataset)
+        dataset = self.act2(self.conv2(dataset))
+        dataset = self.pool1(dataset)
+        dataset = self.flatten(dataset)
+        dataset = self.fc4(dataset)
+        return dataset
 
-fig, axes = plt.subplots(rows, cols, figsize=(15, 8))
-fig.suptitle("Feature Maps from First Convolutional Layer")
 
-for i in range(rows * cols):
-    ax = axes[i // cols, i % cols]
-    ax.imshow(imm[0, i].detach().numpy(), cmap='gray')
-    ax.axis("off")
-    ax.set_title(f"Filter {i}")
+# Model Training
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = Fer2013().to(device)
 
-plt.tight_layout()
-plt.show()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+n_epochs = 20
+
+for epoch in range(n_epochs):
+    model.train()
+    for inputs, labels in trainloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        y_pred = model(inputs)
+        loss = loss_fn(y_pred, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    model.eval()
+    acc = 0
+    count = 0
+    with torch.no_grad():
+        for inputs, labels in testloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            y_pred = model(inputs)
+            acc += (torch.argmax(y_pred, 1) == labels).float().sum()
+            count += len(labels)
+
+    acc /= count
+    print(f"Epoch {epoch + 1}: Model Accuracy {acc * 100:.2f}%")
